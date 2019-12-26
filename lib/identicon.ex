3f5f720d3d10ba49151319a22d30f31822a7e1d7
jngd <juangrdiaz@gmail.com>
2019-12-26 16:55:48 +0100

    Grid generated

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -12,9 +13,17 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b |_tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b |_tail]} = image) do
+    %Identicon.Image{image |color: {r,g,b}}
+  end
 
-    [r, g, b]
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row([first, second |_tail] = row) do
+    row ++ [second, first]
   end
 end
